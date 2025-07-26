5410890bdacc8c05d275e45c24dddbaf8c09d47a
Sam Minamioka <sminamioka@gmail.com>
2025-07-26 21:05:23 +0100

    add

---
+++ SQL/Cars_Project/Source_Cars_Data_Table.sql
@@ -0,0 +1,13 @@
+Create table source_cars_data (
+Branch_ID Varchar(200),
+Dealer_ID Varchar(200),
+Model_ID Varchar(200),
+Revenue Bigint,
+Units_Sold Bigint,
+Date_ID Varchar(200),
+Day int,
+Month int,
+Year int,
+BranchName Varchar(200),
+DealerName Varchar(200)
+)
